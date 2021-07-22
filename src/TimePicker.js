fcdacc939ba56ab3b9e57da0a2de96ee21770b5f
MariJumava <marfavasilevna@inbox.ru>
2021-07-22 12:13:24 +0300

    init: TimePicker

--- src/TimePicker.js
+++ src/TimePicker.js
@@ -1,22 +1,41 @@
 import { render } from "@testing-library/react";
 import { useState, useEffect } from "react";
 
-const TimePicker = (props) => {
+const TimePicker = () => {
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
 
-    const [hour, setHour] = useState(0)
-    const [minute, setMinute] = useState(0)
+  useEffect(() => {
+    if (hour > 23) {
+      setHour(0);
+    }
+    if (hour < 0) {
+      setHour(23);
+    }
+    if (minute > 59) {
+      setMinute(0);
+      setHour(hour + 1);
+    }
+    if (minute < 0) {
+      setMinute(59);
+      setHour(hour - 1);
+    }
+  }, [hour, minute]);
 
-    useEffect(() => {
+  return (
+    <div className="time-picker-container">
+      <div>
+      {hour}
+        <button onClick={() => setHour(hour + 1)}>+</button>
+        <button onClick={() => setHour(hour - 1)}>-</button>
+      </div>
+      <div>
+      {minute}
+        <button onClick={() => setMinute(minute + 1)}>+</button>
+        <button onClick={() => setMinute(minute - 1)}>-</button>
+      </div>
+    </div>
+  );
+};
 
-        
-
-    }, [hour, minute]);
-
-
-        return (
-            <div className='time-picker-container'>
-
-            </div>
-        )
-
-}
+export default TimePicker;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Input from './Binding';
+import TimePicker from './TimePicker';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <TimePicker />,
     <App />,
     <Input />,
   </React.StrictMode>,
